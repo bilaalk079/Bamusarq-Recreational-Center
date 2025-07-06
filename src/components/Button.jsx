5cc7d6439b8dab9faa1f5aafc2279bcbf16a6a6f
bilaalk079 <kolawolebilaal13@gmail.com>
2025-07-06 15:57:03 +0100

    Optimized routing speed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Link from "next/link";
-const Button = ({content,href}) => {
-  return (
-    <div className="w-1/2 mx-auto">
-      <button className="bg-green-700 hover:bg-green-800 rounded-xl p-5 mt-2 text-lg sm:text-3xl md:text-2xl lg:text-2xl mb-1 text-white font-bold w-full animate-bounce">
-        <Link href={href} >{content}</Link >
-      </button>
-    </div>
-  );
+const Button = ({ content, href }) => {
+     return (
+          <div className="w-1/2 mx-auto">
+               <button
+                    onClick={(window.location.href = href)}
+                    className="bg-green-700 hover:bg-green-800 rounded-xl p-5 mt-2 text-lg sm:text-3xl md:text-2xl lg:text-2xl mb-1 text-white font-bold w-full animate-bounce"
+               >
+                    <h1>{content}</h1>
+               </button>
+          </div>
+     );
 };
 
 export default Button;
