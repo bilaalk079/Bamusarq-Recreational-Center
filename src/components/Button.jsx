f370c81f036d139a32a1d584cf36eef53aeb93f0
bilaalk079 <kolawolebilaal13@gmail.com>
2025-07-06 16:53:25 +0100

    Fixed a minor bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
+'use client'
 import React from "react";
+import { useRouter } from "next/navigation";
+
 const Button = ({ content, href }) => {
+     const router = useRouter
      return (
           <div className="w-1/2 mx-auto">
                <button
-                    onClick={(window.location.href = href)}
+                    onClick={() => router.push(href)}
                     className="bg-green-700 hover:bg-green-800 rounded-xl p-5 mt-2 text-lg sm:text-3xl md:text-2xl lg:text-2xl mb-1 text-white font-bold w-full animate-bounce"
                >
                     <h1>{content}</h1>
